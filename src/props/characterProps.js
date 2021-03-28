90131c26c6fb956f2cba2fe23efcd77cc55ef86e
Miguel Almeida <m.almeida@runtime-revolution.com>
2021-03-28 19:25:11 +0100

    [ch-2/ch-3] Add character and walking

---
+++ src/props/characterProps.js
@@ -0,0 +1,4 @@
+export const defaultCharacter = {
+  model: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png",
+  shadow: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png"
+}
